f211fba347fb65e71e9c32b9cc542ef3113e5cfe
samuelretana-sare <samuel.retana@veronicasins.com>
2026-06-22 14:49:41 -0600

    Primer push con estructura base de proyeto y configuracion de gitignore

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// ============================================================
+// SISTEMA DE INVENTARIO - Clase 1.1
+// Estado: Mensaje de bienvenida
+// ============================================================
+
+using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+var version = assembly.GetName().Version;
+
+
+
+Console.WriteLine("==========================================");
+Console.WriteLine("    SISTEMA DE GESTIÓN DE INVENTARIO      ");
+Console.WriteLine("==========================================");
+Console.WriteLine();
+Console.WriteLine($"Versión: {version}");
+Console.WriteLine($"Plataforma: {Environment.OSVersion}");
+Console.WriteLine($".NET Version: {Environment.Version}");
+Console.WriteLine();
+Console.WriteLine("estrructura dle proyecto:");
+Console.WriteLine("configuración .csproj:");
+Console.WriteLine("Carpeta src/ creada");
+Console.WriteLine("Metadatos configurados:");
+Console.WriteLine();
+Console.WriteLine("Proximo paso: Agregar argumentos CLI y Configuracion del repositorio Github");
